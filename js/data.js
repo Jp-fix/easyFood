1d7c40b0fcb6f8535fe39fffd7d00926e4333cb8
Jp-fix <jpatrick.dev@pm.me>
2025-03-11 14:02:50 +0100

    recipe form and logic

--- js/data.js
+++ js/data.js
@@ -1,24 +1,82 @@
-class recipe {
-    constructor (iD = null, recipeName = "Pas de nom", recipeType = "Pas de type", difficulty = "Pas de difficulté", prepTime = [], ingredients = [], prepSteps = [], image = null, comments = null){
+document.getElementById("addNewRecipe").addEventListener("submit", function(e) {
+    e.preventDefault()
+    let recipeId = self.crypto.randomUUID()
+    let getRecipeDifficultyData = document.getElementById("recipeDifficulty")
+    let recipeDifficulty = getRecipeDifficultyData.value
+    let recipeImage = document.getElementById("recipeImage")
+    let recipePrepTime = document.getElementById("prepTime").value
+    let recipeType = document.getElementById("recipeType").value
+    let recipeName = document.getElementById("recipeName").value
+
+    let getNewRecipeFormData = new Recipe(
+        recipeId,
+        recipeName,
+        recipeType,
+        recipeDifficulty,
+        recipePrepTime,
+        recipeAllIngredients,
+        recipeAllSteps,
+        recipeImage,
+    )
+
+    console.log(`Ajout d'une nouvelle recette à la bdd : ${getNewRecipeFormData.recipeName}`)
+    console.log(getNewRecipeFormData)
+
+    this.reset()
+    recipeAllIngredients = [];
+    recipeAllSteps = [];
+
+    console.log("Formulaire réinitialisé !")
+})
+
+
+// Stockage des données imbriquées (allIngrédients + allSteps)
+let recipeAllIngredients = []
+let recipeAllSteps = []
+
+export class Recipe {
+    constructor (iD = null, recipeName = "Pas de nom", recipeType = "Pas de type", recipeDifficulty = "Pas de difficulté", recipePrepTime = [], recipeAllIngredients = [], recipeAllSteps = [], image = null){
         this.iD = iD;
         this.recipeName = recipeName;
         this.recipeType = recipeType;
-        this.difficulty = difficulty;
-        this.prepTime = prepTime || [];
-        this.ingredients = ingredients || [];
-        this.prepSteps = prepSteps || [];
+        this.recipeDifficulty = recipeDifficulty;
+        this.recipePrepTime = recipePrepTime || [];
+        this.recipeAllIngredients = recipeAllIngredients || [];
+        this.recipeAllSteps = recipeAllSteps || [];
         this.image = image;
-        this.comments = comments;
-    }
-    addIngredient(name, quantity, unit){
-        this.ingredients.push({name, quantity, unit})
-    }
-    addPrepTime(timing, timeUnit){
-        this.prepTime.push({timing, timeUnit})
     }
 }
+// Ajout des ingrédients de la recette
+document.getElementById("addNewIngredient").addEventListener("click", function (e){
 
-const couscous = new recipe (
+    let ingredientName = document.getElementById("ingredientName").value
+    let ingredientQuantity = document.getElementById("ingredientQuantity").value
+    let ingredientUnit = document.getElementById("ingredientUnit").value
+    let completIngredient = `${ingredientName} ${ingredientQuantity} ${ingredientUnit}`
+    recipeAllIngredients.push(completIngredient)
+
+    document.getElementById("ingredientName").value = ""
+    document.getElementById("ingredientQuantity").value = ""
+    document.getElementById("ingredientUnit").value = ""
+
+    console.log("All ingredient array:",recipeAllIngredients)
+})
+
+// Ajout des étapes de la recette
+document.getElementById("addNewStep").addEventListener("click", function (e){
+
+    let stepName = document.getElementById("stepName").value
+    let stepDesc = document.getElementById("stepDesc").value
+    let completStep = `${stepName} ${stepDesc}`
+    recipeAllSteps.push(completStep)
+
+    document.getElementById("stepName").value = ""
+    document.getElementById("stepDesc").value = ""
+
+    console.log("All steps array:",recipeAllSteps)
+})
+
+/*const couscous = new Recipe (
     1,
     "Couscous",
     "Gourmand",
@@ -28,16 +86,25 @@ couscous.addPrepTime(90,"minutes")
 couscous.addIngredient("semoule", 500,"grammes")
 couscous.addIngredient("Poulet", 350,"grammes")
 couscous.addIngredient("Merguez", 6,"Unités")
-
-console.log(`Pour faire un bon ${couscous.recipeName.toString()} il faut ${couscous.ingredients[0].quantity} ${couscous.ingredients[0].unit} de ${couscous.ingredients[0].name}. Il faut aussi avoir ${couscous.prepTime[0].timing} ${couscous.prepTime[0].timeUnit} devant soit.`)
+couscous.addPrepSteps(
+    "Faire cuire la semoule",
+    "Ajouter les légumes",
+    "Ajouter la viande",
+    "Servir chaud",
+    )*/
 /*
-* name = Nom de la recette
-* recipeType = Healthy, Gourmande
-* difficulty = Facile, Moyenne, Hard
-* prepTime = Temps de préparation
-* ingredients = Tableau d'ingrédients nécessaires
-* quantity = Quantité des ingrédients
-* prepSteps = Étapes de préparation
-* image = Image d'illustration de la recette
-* comments = Retour d'expérience et axes d'améliorations
+
+console.log(`
+Pour faire un bon ${couscous.recipeName.toString()} !
+Il faut ${couscous.ingredients[0].quantity} ${couscous.ingredients[0].unit} de ${couscous.ingredients[0].name}.
+
+Liste des étapes à réaliser :
+
+ - Étape 1 : ${couscous.prepSteps[0].step1}
+ - Étape 2 : ${couscous.prepSteps[0].step2}
+ - Étape 3 : ${couscous.prepSteps[0].step3}
+ - Étape 4 : ${couscous.prepSteps[0].step4}
+
+ Profitez de votre repas !
+ `)
 */
